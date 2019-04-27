8c26164c8e68786f2c1ce452f58b6ed57157bd70
Desiree Surjadi <fireskywalker@gmail.com>
2019-04-27 13:51:38 +1000

    Remove old resume

--- resume/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './index.css';
-import Resume from './containers/Resume';
-import registerServiceWorker from './utils/registerServiceWorker';
-
-ReactDOM.render(<Resume />, document.getElementById('root'));
-registerServiceWorker();
